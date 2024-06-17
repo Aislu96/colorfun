fb4a6fb05ebb174e55761ca94ae86544e24d0111
aislusha96 <arslanova1996ais@list.ru>
2024-06-17 10:49:27 +0300

    коммит

--- src/js/common.js
+++ src/js/common.js
@@ -8,5 +8,17 @@ for (let i = 0; i < buttonArrow.length; i++) {
   buttonArrow[i].addEventListener('mouseout', function () {
     gridContainerGroupArticles[i].classList.remove('hovered');
   });
-
 }
+
+
+
+
+// клик по кнопке up-arrow
+const upArrow = document.querySelector('.up-arrow');
+upArrow.addEventListener('click', () => {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+});
+
+upArrow.addEventListener('touched', () => {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+});
