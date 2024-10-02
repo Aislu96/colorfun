272120123b42cc78bccb83386a3869b3907539b0
Aislu Kharisova <arslanova1996ais@list.ru>
2024-10-02 12:46:59 +0300

    "исправления"

--- frontend/src/index.js
+++
@@ -1,8 +0,0 @@
-import "./js/common.js";
-import "./js/cards.js";
-import "./js/savePicture.js";
-import "./js/swiper.js";
-import "./js/app.js";
-import "./js/telephone.js";
-import "./assets/css/main.css";
-import "./assets/scss/main.scss";
